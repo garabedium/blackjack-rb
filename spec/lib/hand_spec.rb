b72d6463b4b3eade1fc46bc17e9823a0a9ee81eb
Greg G <garabedium@users.noreply.github.com>
2023-05-01 16:49:52 -0400

    fix hand text test

--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -1,18 +1,18 @@
-require "spec_helper"
-require_relative "../../lib/deck"
-require_relative "../../lib/card"
-require_relative "../../lib/hand"
+require 'spec_helper'
+require_relative '../../lib/deck'
+require_relative '../../lib/card'
+require_relative '../../lib/hand'
 
 describe Hand do
   let(:deck) { Deck.new }
   let(:hand) { Hand.new }
   let(:cards) { deck.deck.shift(2) }
 
-  it "shows the current cards in the hand" do
+  it 'shows the current cards in the hand' do
     expect(hand.cards.size).to eq(0)
     hand.cards.push(cards[0], cards[1])
     expect(hand.cards.size).to eq(2)
-    cards_text = "#{cards[0].text} #{cards[1].text} "
+    cards_text = "#{cards[0].text} #{cards[1].text}"
     expect(hand.text).to eq(cards_text)
   end
 
